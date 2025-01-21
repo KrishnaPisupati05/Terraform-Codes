83ca013c6fcdaf096eed7bb65903f2cdf64bd70c
KrishnaPisupati05 <krishna.pisupati@sas.dk>
2025-01-21 12:09:38 +0530

    Add the files

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   resource_location = "Central India"
-  virtual_network={
-    name="app-network"
-    address_prefixes=["10.0.0.0/16"]
-  }
+  # virtual_network={
+  #   name="app-network"
+  #   address_prefixes=["10.0.0.0/16"]
+  # }
 }
 #   subnets=[
 #     {
--- main.tf
+++ main.tf
@@ -3,69 +3,108 @@ resource "azurerm_resource_group" "appgrp" {
   location = local.resource_location
 }
 
-resource "azurerm_virtual_network" "app_network" {
-  name                = local.virtual_network.name
-  location            = local.resource_location
+resource "azurerm_service_plan" "serviceplan" {
+  for_each = var.webapp_environment.production.serviceplan
+  name                = each.key
   resource_group_name = azurerm_resource_group.appgrp.name
-  address_space       = local.virtual_network.address_prefixes 
+  location            = local.resource_location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku
 }
 
-resource "azurerm_subnet" "app_network_subnets" {
-  for_each = {
-    "websubnet01" = ["10.0.0.0/24"]
-    "appsubnet01" = ["10.0.1.0/24"]
+resource "azurerm_windows_web_app" "webapp" {
+  for_each = var.webapp_environment.production.serviceapp
+  name                = each.key
+  resource_group_name = azurerm_resource_group.appgrp.name
+  location            = local.resource_location
+  service_plan_id     = azurerm_service_plan.serviceplan[each.value].id
+  
+  site_config {
+    always_on=false
+     
+    application_stack {
+      current_stack="dotnet"
+      dotnet_version="v8.0"
   }
-  name                 = each.key
-  resource_group_name  = azurerm_resource_group.appgrp.name
-  virtual_network_name = azurerm_virtual_network.app_network.name
-  address_prefixes     = each.value
+  }  
 }
 
-resource "azurerm_network_interface" "webinterfaces" {
-  count = 2
-  name                = "webinterface0${count.index+1}"
-  location            = local.resource_location
-  resource_group_name = azurerm_resource_group.appgrp.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.app_network_subnets["websubnet01"].id # Need to take the key value here
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.webip[count.index].id
-  }
-}
 
-resource "azurerm_public_ip" "webip" {
-  count = 2
-  name                = "webip0${count.index+1}"
-  resource_group_name = azurerm_resource_group.appgrp.name
-  location            = local.resource_location
-  allocation_method   = "Static"
-}
 
-resource "azurerm_network_security_group" "app_nsg" {
-  name                = "app-nsg"
-  location            = local.resource_location
-  resource_group_name = azurerm_resource_group.appgrp.name
 
-  security_rule {
-    name                       = "AllowRDP"
-    priority                   = 300
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
 
-resource "azurerm_subnet_network_security_group_association" "subnet_appnsg" {
-  for_each = azurerm_subnet.app_network_subnets
-  subnet_id                 = azurerm_subnet.app_network_subnets[each.key].id   # Taking each.key from the subnet
-  network_security_group_id = azurerm_network_security_group.app_nsg.id
-}
+
+
+
+
+
+
+
+
+# resource "azurerm_virtual_network" "app_network" {
+#   name                = local.virtual_network.name
+#   location            = local.resource_location
+#   resource_group_name = azurerm_resource_group.appgrp.name
+#   address_space       = local.virtual_network.address_prefixes 
+# }
+
+# resource "azurerm_subnet" "app_network_subnets" {
+#   for_each = {
+#     "websubnet01" = ["10.0.0.0/24"]
+#     "appsubnet01" = ["10.0.1.0/24"]
+#   }
+#   name                 = each.key
+#   resource_group_name  = azurerm_resource_group.appgrp.name
+#   virtual_network_name = azurerm_virtual_network.app_network.name
+#   address_prefixes     = each.value
+# }
+
+# resource "azurerm_network_interface" "webinterfaces" {
+#   count = 2
+#   name                = "webinterface0${count.index+1}"
+#   location            = local.resource_location
+#   resource_group_name = azurerm_resource_group.appgrp.name
+
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.app_network_subnets["websubnet01"].id # Need to take the key value here
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id = azurerm_public_ip.webip[count.index].id
+#   }
+# }
+
+# resource "azurerm_public_ip" "webip" {
+#   count = 2
+#   name                = "webip0${count.index+1}"
+#   resource_group_name = azurerm_resource_group.appgrp.name
+#   location            = local.resource_location
+#   allocation_method   = "Static"
+# }
+
+# resource "azurerm_network_security_group" "app_nsg" {
+#   name                = "app-nsg"
+#   location            = local.resource_location
+#   resource_group_name = azurerm_resource_group.appgrp.name
+
+#   security_rule {
+#     name                       = "AllowRDP"
+#     priority                   = 300
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "3389"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+# }
+
+# resource "azurerm_subnet_network_security_group_association" "subnet_appnsg" {
+#   for_each = azurerm_subnet.app_network_subnets
+#   subnet_id                 = azurerm_subnet.app_network_subnets[each.key].id   # Taking each.key from the subnet
+#   network_security_group_id = azurerm_network_security_group.app_nsg.id
+# }
 
 
 # resource "azurerm_subnet" "websubnet01" {
