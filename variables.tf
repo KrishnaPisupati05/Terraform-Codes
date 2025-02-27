66178380b6e9dbaa5240747d21a9f42d3cdc9b5b
KrishnaPisupati05 <krishna.pisupati@sas.dk>
2025-02-27 14:39:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,1 @@
-variable "webapp_environment" {
-     type = map(object(
-        {
-            serviceplan=map(object(
-                {
-                    sku=string
-                    os_type=string
-                }
-            ))
-            serviceapp=map(string)
-        }
-     ))
-}
 
